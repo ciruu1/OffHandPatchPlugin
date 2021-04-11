b164d6d3d10dcbe0e4b07136741a3a2274636bc7
Ciruu <ciruu@protonmail.com>
2021-04-11 17:08:41 +0200

    Patched "Sword + Golden Apple" and canceled the crystal placement with offhand while eating

--- src/main/java/me/ciruu/offhandpatch/Offhandpatch.java
+++ src/main/java/me/ciruu/offhandpatch/Offhandpatch.java
@@ -4,7 +4,9 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,12 +28,32 @@ public final class Offhandpatch extends JavaPlugin implements Listener {
             Player player = (Player) event.getDamager();
             ItemStack mainHand = player.getInventory().getItemInMainHand();
             ItemStack offHand = player.getInventory().getItemInOffHand();
-            if (player.getActiveItem().getType() == Material.GOLDEN_APPLE &&
-                    ((offHand.getType() == Material.END_CRYSTAL && mainHand.getType() == Material.GOLDEN_APPLE) ||
-                    (mainHand.getType() == Material.END_CRYSTAL && offHand.getType() == Material.GOLDEN_APPLE))
-            ) {
+            if (cancelAttack(mainHand.getType(), offHand.getType(), player))
                 event.setCancelled(true);
-            }
         }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Player player = event.getPlayer();
+            ItemStack mainHand = player.getInventory().getItemInMainHand();
+            ItemStack offHand = player.getInventory().getItemInOffHand();
+
+            // Make sure that the player is interacting with an end crystal
+            if (event.getMaterial() == Material.END_CRYSTAL && cancelPlace(mainHand.getType(), offHand.getType(), player))
+                event.setCancelled(true);
+        }
+    }
+
+    private boolean cancelAttack(Material mainHand, Material offHand, Player player) {
+        return ((mainHand == Material.GOLDEN_APPLE && offHand == Material.END_CRYSTAL) ||
+                ((mainHand == Material.END_CRYSTAL || mainHand == Material.DIAMOND_SWORD) && offHand == Material.GOLDEN_APPLE)
+                        && player.getActiveItem().getType() == Material.GOLDEN_APPLE);
+    }
+
+    private boolean cancelPlace(Material mainHand, Material offHand, Player player) {
+        return (mainHand == Material.GOLDEN_APPLE && offHand == Material.END_CRYSTAL) ||
+                (mainHand == Material.END_CRYSTAL && offHand == Material.GOLDEN_APPLE && player.getActiveItem().getType() == Material.GOLDEN_APPLE);
     }
 }
